5aa84b8912bf002f467d7d6812fc70b0cfa1685c
Georgi Angelov <the.wizarda@gmail.com>
2023-01-15 12:11:51 +0200

    Add files via upload

---
+++ platform.py
@@ -0,0 +1,7 @@
+# Copyright 2023 (c) WizIO ( Georgi Angelov )
+
+from platformio.managers.platform import PlatformBase
+
+class Xc32Platform(PlatformBase):
+    def is_embedded(self):
+        return True
